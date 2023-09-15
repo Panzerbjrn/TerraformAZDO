5416eee92cbf721f34129d59d70bb2b88487cc30
Lars Petersson <lars.petersson@lexacom.co.uk>
2023-09-15 11:39:33 +0100

    Refreshing

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~> 3"
+    }
+  }
+  cloud {
+    organization = "Panzerbjrn"
+    workspaces {
+      name = "TerraformAZDO"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "resoucegroup" {
+  name     = "TerraformAZDO"
+  location = var.location
+}
